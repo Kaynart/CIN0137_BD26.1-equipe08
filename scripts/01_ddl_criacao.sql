2723402e41d18274212b15a44757c8886ba657a4
Kaynan Roberth <kaynancabj4b@gmail.com>
2026-05-06 09:31:06 -0300

    Ajusta atributos de cache (Diretor, Ator e Figurinista) e adiciona o atributo nome em Estudio
    - As tabelas associadas ao relacionamentos que ligam os especializados e seus trabalhos (Filme, Ator-Filme e Confecciona) recebem o atributo cache
    - Atributo salario removido de Funcionario
    - Adicao do atributo nome para o Estudio

--- scripts/01_ddl_criacao.sql
+++ scripts/01_ddl_criacao.sql
@@ -7,7 +7,7 @@ CREATE SEQUENCE seq_figurino START WITH 1 INCREMENT BY 1;
 -- ================= Entidade - Equipamento =================
 CREATE TABLE equipamento (
 
-        id_equipamento NUMBER DEFAULT seq_equipamento.NEXTVAL,						-- Chave Identificador unico de equipamento // LEMBRAR DE VER SE O NEXTVAL VAI AQUI OU SO NO INSERT
+        id_equipamento NUMBER DEFAULT seq_equipamento.NEXTVAL,						-- Chave Identificador unico de equipamento
         tipo     VARCHAR2(50) NOT NULL,									            -- Tipo do equipamento | ex: Camera, Microfone 
         valor    NUMBER(10, 2),										                -- Preço do equipamento com duas casas decimais
 	
@@ -23,7 +23,7 @@ CREATE TABLE equipamento (
 CREATE TABLE fornecedor (
 
         cnpj_fornecedor VARCHAR2(18),									            -- Chave CNPJ do Fornecedor com 18 espaços contando os pontos, barra e hifen de um CNPJ
-        razao_social VARCHAR2(100),									                -- Razao social associada ao fornecedor, varchar2 'ilimitado'
+        razao_social VARCHAR2(100) NOT NULL,						                -- Razao social associada ao fornecedor, varchar2 'ilimitado'
 
 	    CONSTRAINT pk_fornecedor PRIMARY KEY (cnpj_fornecedor)						-- Uso de CONSTRAINT: regra que define cnpj_fornecedor como PK
 
@@ -33,8 +33,9 @@ CREATE TABLE fornecedor (
 -- ================= Entidade - Estudio =================
 CREATE TABLE estudio (
 	    num_id_estudio NUMBER DEFAULT seq_estudio.NEXTVAL,						    -- Chave Identificador unico de estudio
+		nome_estudio VARCHAR2(100) NOT NULL,										-- Nome do estudio
 	    metragem NUMBER(5, 2),										                -- Comprimento em metragem quadrada do estudio, com duas casas decimais
-	    tem_chroma_key CHAR(1),										                -- Flag indicando S ou N pra existencia de chroma key
+	    tem_chroma_key CHAR(1) NOT NULL DEFAULT 'N',							    -- Flag indicando S ou N pra existencia de chroma key
 
 	    CONSTRAINT pk_estudio PRIMARY KEY (num_id_estudio),						    -- Uso de CONSTRAINT: regra que define num_id_estudio como PK
 
@@ -51,17 +52,14 @@ CREATE TABLE estudio (
 CREATE TABLE funcionario (
 
         cpf VARCHAR2(14),										                                -- Chave CPF do Fornecedor com 14 espaços contando os pontos e hifen de um CPF
-       	nome_funcionario VARCHAR2(100),									                        -- Nome do funcionario
+       	nome_funcionario VARCHAR2(100) NOT NULL,						                        -- Nome do funcionario
 	    sexo CHAR(1),											                                -- Sexo do funcionario para 'M' ou 'F'
-	    salario NUMBER(8, 2),	 									                            -- Salario do funcionario com duas casas decimais e inteiros ate 5 digitos
 	    data_nascimento_funcionario DATE,								                        -- Data de nascimento
 	    cpf_supervisor VARCHAR2(14),									                        -- Estrangeira referenciando funcionario supervisor
 
 	CONSTRAINT pk_funcionario PRIMARY KEY (cpf),							                    -- Uso de CONSTRAINT: regra que define cpf como PK
 	
 	    CONSTRAINT check_sexo CHECK (sexo IN ('F', 'M')),						                -- Uso de CONSTRAINT: checa e garante que o atributo sexo possua apenas 'F' e 'M' como valor
-
-	    CONSTRAINT check_salario CHECK (salario >= 1621),						                -- Uso de CONSTRAINT: checa e garante que o salario registrado seja maior que o salario minimo brasileiro
 
 	    CONSTRAINT fk_supervisor FOREIGN KEY (cpf_supervisor) REFERENCES funcionario(cpf)		-- Uso de CONSTRAINT: regra que define cpf_supervisor como FK referenciando funcionario(cpf)
 
@@ -126,7 +124,6 @@ CREATE TABLE diretor (
 	    CONSTRAINT fk_cpf_diretor FOREIGN KEY (cpf_funcionario) REFERENCES funcionario(cpf)			        -- Uso de CONSTRAINT: regra que define cpf_funcionario como FK referenciando funcionario(cpf)
 
 
-
 );
 
 
@@ -147,7 +144,7 @@ CREATE TABLE figurinista (
 CREATE TABLE ator (
 
 	    cpf_funcionario VARCHAR2(14),										                                -- Estrangeira referenciando funcionario - chave
-	    numero_drt VARCHAR2(20),										                                    -- Numero DRT do ator
+	    numero_drt VARCHAR2(20) NOT NULL,								                                    -- Numero DRT do ator
 
 	    CONSTRAINT pk_ator PRIMARY KEY (cpf_funcionario),							                        -- Uso de CONSTRAINT: regra que define cpf_funcionario como PK
 
@@ -168,15 +165,17 @@ CREATE TABLE filme (
 	    ano_lancamento NUMBER(4),										                                            -- Ano em que o filme foi lancado
 	    titulo VARCHAR2(100) NOT NULL,										                                        -- Titulo
 	    cpf_diretor VARCHAR2(14) NOT NULL,									                                        -- Estrangeira referenciando Diretor que dirigiu o filme
-
+		cache_diretor NUMBER(12,2),											                                        -- Cache recebido pelo diretor na direcao desse filme
+	
 	    CONSTRAINT pk_filme PRIMARY KEY (id_filme),								                                    -- Uso de CONSTRAINT: regra que define id_filme como PK
 
 	    CONSTRAINT fk_diretor FOREIGN KEY (cpf_diretor) REFERENCES diretor(cpf_funcionario),			            -- Uso de CONSTRAINT: regra que define cpf_diretor como FK referenciando diretor(cpf_funcionario)
 
 	    CONSTRAINT check_class_indic CHECK (classificacao_indicativa IN ('L', '10', '12', '14', '16', '18')),	    -- Uso de CONSTRAINT: checa e garante que classificacao indicativa e valida dentre as usadas no ambito cinematografico
 
-	    CONSTRAINT check_ano_lancamento CHECK (ano_lancamento >= 1895)                                              -- Uso de CONSTRAINT: checa e garante que o ano de lancamento e valido (primeiro filme lancado em 1895)
+	    CONSTRAINT check_ano_lancamento CHECK (ano_lancamento >= 1895),                                             -- Uso de CONSTRAINT: checa e garante que o ano de lancamento e valido (primeiro filme lancado em 1895)
 
+		CONSTRAINT check_cache_diretor CHECK (cache_diretor > 0)													-- Uso de CONSTRAINT: checa e garante que o cache do diretor e valido (maior que 0)
 );
 
 
@@ -188,7 +187,7 @@ CREATE TABLE ator_filme (
 	    cpf_ator VARCHAR2(14),											                                            -- Estrangeira referenciando ator compondo a chave com o id_filme
 	    id_filme NUMBER,											                                                -- Estrangeira referenciando filme compondo a chave com o cpf_ator
 	    personagem VARCHAR2(100) NOT NULL,									                                        -- Personagem interpretado nessa atuacao
-	    cache NUMBER(12,2),											                                                -- Cache recebido
+	    cache_ator NUMBER(12,2),											                                        -- Cache recebido pelo ator nesse filme
 
 	    CONSTRAINT pk_ator_filme PRIMARY KEY (cpf_ator, id_filme),						                            -- Uso de CONSTRAINT: regra que define cpf_ator e id_filme funcionario como PK
 	
@@ -196,7 +195,7 @@ CREATE TABLE ator_filme (
 
 	    CONSTRAINT fk_2_ator_filme FOREIGN KEY (id_filme) REFERENCES filme(id_filme),				                -- Uso de CONSTRAINT: regra que define id_filme como FK referenciando filme(id_filme)
 
-	    CONSTRAINT check_cache CHECK (cache > 0)								                                    -- Uso de CONSTRAINT: checa e garante que o cache e maior que 0
+	    CONSTRAINT check_cache_ator CHECK (cache_ator > 0)								                            -- Uso de CONSTRAINT: checa e garante que o cache e maior que 0
 
 );
 
@@ -214,11 +213,11 @@ CREATE TABLE figurino (
 
 	    CONSTRAINT pk_figurino PRIMARY KEY (id_figurino),							                                -- Uso de CONSTRAINT: regra que define id_figurino funcionario como PK
 	
-	    CONSTRAINT fk_1_figurino FOREIGN KEY (cpf_ator_vestido) REFERENCES ator(cpf_funcionario),		            -- Uso de CONSTRAINT: regra que define cpf_ator como FK referenciando ator(cpf_funcionario)
+	    CONSTRAINT fk_1_figurino FOREIGN KEY (cpf_ator_vestido) REFERENCES ator(cpf_funcionario),		            -- Uso de CONSTRAINT: regra que define cpf_ator_vestido como FK referenciando ator(cpf_funcionario)
 
-	    CONSTRAINT fk_2_figurino FOREIGN KEY (id_filme_vestido) REFERENCES filme(id_filme),			                -- Uso de CONSTRAINT: regra que define id_filme como FK referenciando filme(id_filme)
+	    CONSTRAINT fk_2_figurino FOREIGN KEY (id_filme_vestido) REFERENCES filme(id_filme),			                -- Uso de CONSTRAINT: regra que define id_filme_vestido como FK referenciando filme(id_filme)
 
-	    CONSTRAINT check_tamanho CHECK (tamanho IN ('PP', 'P', 'M', 'G', 'GG', 'XGG'))				                -- Uso de CONSTRAINT: checa e garante que o cache e maior que 0
+	    CONSTRAINT check_tamanho CHECK (tamanho IN ('PP', 'P', 'M', 'G', 'GG', 'XGG'))				                -- Uso de CONSTRAINT: checa e garante que o tamanho seja dentro do padrao de descricao
 
 );
 
@@ -246,13 +245,15 @@ CREATE TABLE confecciona (
 	
 	    cpf_figurinista VARCHAR2(14),										                                -- Estrangeira referenciando figurinista compondo a chave com id_figurino
 	    id_figurino NUMBER,											                                        -- Estrangeira referenciando figurino compondo a chave com cpf_figurinista
-
+		cache_figurinista NUMBER(12,2),											                            -- Cache recebido pelo figurinista nessa confeccao de figurino
+	
 	    CONSTRAINT pk_confecciona PRIMARY KEY (cpf_figurinista, id_figurino),					            -- Uso de CONSTRAINT: regra que define id_filme e genero funcionario como PK
 
 	    CONSTRAINT fk_figurinista FOREIGN KEY (cpf_figurinista) REFERENCES figurinista(cpf_funcionario),	-- Uso de CONSTRAINT: regra que define cpf_figurinista como FK referenciando figurinista(cpf_funcionario)
 
 	    CONSTRAINT fk_figurino FOREIGN KEY (id_figurino) REFERENCES figurino(id_figurino)			        -- Uso de CONSTRAINT: regra que define id_figurino como FK referenciando figurino(id_figurino)
 
+		CONSTRAINT check_cache_figurinista CHECK (cache_figurinista > 0)									-- Uso de CONSTRAINT: checa e garante que o cache do figurinista e valido (maior que 0)
 );
 
 
